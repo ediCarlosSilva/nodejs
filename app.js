d233362d19bf2489b8dcf199bf7b38204801b88c
edicarlos <edicarlos@debianPC>
2018-03-25 10:06:55 -0300

    testes com nodemon

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/produtos', function(req, res) {
+    console.log('atendendo a requisição');
+    console.log('listando...');
     res.render('produtos/lista');
 });
 
