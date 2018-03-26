f2f848045a439443c3675b1e737e7a726216ba4f
Edi Carlos note <edi_csilva@hotmail.com>
2018-03-26 20:09:41 -0300

    criado a view para o formulario intallado a dependencia body-parser

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -6,6 +6,11 @@ ProdutosDAO.prototype.lista = function(callback) {
     this._connection.query('select * from livros', callback);
 }
 
+ProdutosDAO.prototype.salva = function(produto, callback) {
+    this._connection.query('insert into livros set ?', produto, callback);
+    // this._connection.query('insert into livros (titulo, preco, descricao) values (?, ?, ?)', [produto.titulo, produto.preco, produto.descricao], callback);
+}
+
 module.exports = function() {
     return ProdutosDAO;
 }
