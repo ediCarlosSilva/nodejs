44ce9807ba7c61aebbab9fff3412ba4447043eed
= <=>
2018-03-25 21:13:22 -0300

    criado conexao com o banco de dados, usado retono no ejs

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/produtos', function(req, res) {
-    console.log('atendendo a requisição');
-    console.log('listando...');
-    res.render('produtos/lista');
-});
+var app = require('./config/express')();
+var rotasProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000, function() {
     console.log('Servidor rodando');
