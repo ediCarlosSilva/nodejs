60528f03fc6519d93f67459005bb23a8928e39b7
Edi Carlos note <edi_csilva@hotmail.com>
2018-03-27 20:39:30 -0300

    feito validacao com express-validator

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -6,10 +6,10 @@ module.exports = function(app) {
 
         produtosDAO.lista(function(erros, resultados) {
             res.format({
-                html : function() {
+                html: function() {
                     res.render('produtos/lista', { lista: resultados });
                 },
-                json : function() {
+                json: function() {
                     res.json(resultados);
                 }
             })
@@ -20,14 +20,32 @@ module.exports = function(app) {
     });
 
     app.get('/produtos/form', function(req, res) {
-        res.render('produtos/form')
+        res.render('produtos/form', { errosValidacao: {}, produto: {} })
     });
 
     app.post('/produtos', function(req, res) {
 
         var produto = req.body;
-        console.log(produto);
+        // console.log(produto);
 
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
+
+        var erros = req.validationErrors();
+
+        if (erros) {
+            console.log(erros);
+            res.status(400);
+            res.format({
+                html: function() {
+                    res.render('produtos/form', { errosValidacao: erros, produto: produto });
+                },
+                json: function() {
+                    res.json(erros);
+                }
+            });
+            return;
+        }
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
--- cadastra-livros-terminal.js
+++ cadastra-livros-terminal.js
@@ -1,13 +1,13 @@
 var http = require('http');
 
 var configuracoes = {
-    hostname : 'localhost',
-    port : 3000,
-    method : 'post',
-    path : '/produtos',
-    headers : {
-        'Accept' : 'application/json',
-        'Content-type' : 'application/json'
+    hostname: 'localhost',
+    port: 3000,
+    method: 'post',
+    path: '/produtos',
+    headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
     }
 }
 
@@ -19,9 +19,9 @@ var client = http.request(configuracoes, function(res) {
 });
 
 var produto = {
-    titulo : 'mais sobre node',
-    descricao : 'node, javascript e um pouco sobre http',
-    preco : '100'
+    titulo: '',
+    descricao: 'node, javascript e um pouco sobre http',
+    preco: '100'
 }
 
 client.end(JSON.stringify(produto));
