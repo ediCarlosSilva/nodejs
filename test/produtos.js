483aebb7e0618e7c4b18477e100b68f42b04538e
Edi Carlos note <edi_csilva@hotmail.com>
2018-03-28 17:01:37 -0300

    preparação das configurações para os primeiros testes com mocha

---
+++ test/produtos.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var assert = require('assert');
+
+describe('#ProdutosController', function() {
+
+    it('#listagem json', function(done) {
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+
+        http.get(configuracoes, function(res) {
+            if (res.statusCode == 200) {
+                console.log('status ta ok');
+            }
+
+            if (res.headers['content-type'] == 'application/json; charset=utf-8') {
+                console.log('Content type ok');
+            }
+            done();
+        });
+    });
+});
