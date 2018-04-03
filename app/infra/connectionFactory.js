48c95b62fd0aeb0d9e076bac92bb896cca5099be
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-03 15:49:38 -0300

    adicionado configuracoes para deploy no heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -7,7 +7,7 @@ function createDBConnection() {
             host: 'localhost',
             user: 'root',
             password: '',
-            database: 'casadocodigo_nodejsxxx'
+            database: 'casadocodigo_nodejs'
         });
 
     }
@@ -19,6 +19,21 @@ function createDBConnection() {
             user: 'root',
             password: '',
             database: 'casadocodigo_nodejs_test'
+        });
+    }
+
+    if (process.env.NODE_ENV == 'production') {
+        //mysql://bd7e3f3c10380d:aa91407f@us-cdbr-iron-east-05.cleardb.net/heroku_cc187a5c1febd89?reconnect=true
+        var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+        var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/)
+
+
+
+        return mysql.createConnection({
+            host: grupos[3],
+            user: grupos[1],
+            password: grupos[2],
+            database: grupos[4]
         });
     }
 
---
+++ criaTabela.sql
@@ -0,0 +1,6 @@
+create table produtos (
+    id int(11) not null primary key auto_increment,
+    titulo varchar(255) default null,
+    descricao text,
+    preco decimal(10, 2) default null
+);
